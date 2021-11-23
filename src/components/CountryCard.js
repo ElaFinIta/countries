d62c71235c83c59287f817c67f9d5dc3d43614ef
Ele Na <elena.china-kolehmainen@edu.bc.fi>
2021-11-23 22:43:39 +0200

    refactoring class and functional components, add CountryCard, hit my head against the wall making arrays out of objects. There must be another way...

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import '../App.css';
+
+const number = require('easy-number-formatter')
 
 const CountryCard = (props) => {
     return (
-        <div className="card">
-            <h2>{props.name}</h2>
-            {this.state.data.map((item) => (
-                <div key={item.name.common}>
-            <p><img src={item.flags.svg} alt="flag"/> {item.name.common} capital is {item.capital}. Name/s of the country in the native language/s: 
-            </p>
-        </div>
+        <>
+            <div className="card" key={props.name}>
+                {Object.entries(props.currencies).map((currency) => console.log(currency[1].name))}
+                <img src={props.flag} alt="flag"/>
+                {<h2>{props.name}</h2>}
+                {/* <h3 key={props.code}><span>{props.name}</span></h3> */}
+
+                <ul className="languages"> Official languages:
+
+                {/*Inside languages array, every language has an array with language-code and language  */}
+                  {props.languages.map(([code, lang]) => (
+                    <li key={code}>
+                        <span className="bold">{lang}</span>
+                    </li>
+                  ))}
+                </ul>
+
+                <p className="capital">Capital: <span className="bold">{props.capital}</span></p>
+                <p>Population: <span className="bold">{number.formatNumber(props.population)}</span></p>
+
+                <ul className="currencies">Currencies:
+
+                {/*Inside currencies array, every currency is an object with NAME and SYMBOL. Make an array out of an object with Object.entries(yourObject)  */}
+                    {Object.entries(props.currencies).map((currency) => (
+                        <li key={currency[0]}>
+                            <span>{currency[1].name}: </span>
+                            <span className="bold">{currency[1].symbol} </span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 };
 
